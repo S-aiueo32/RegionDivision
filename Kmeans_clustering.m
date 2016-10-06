8129e0b061a00baa3eb426eb9bdded2f487a63ef
内田奏 <14ec020@ms.dendai.ac.jp>
2016-10-06 11:17:43 +0900

    add files

---
+++ Kmeans_clustering.m
@@ -0,0 +1,37 @@
+%K-Means Clustering
+
+clear; close all;
+
+img = imread('convexhull.jpg');
+obj = 3;    %îwåiçûÇ›ÇÃï™äÑêî
+%imshow(img);
+
+cform = makecform('srgb2lab');
+lab_img = applycform(img,cform);
+
+ab = double(lab_img(:,:,2:3));
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+
+nColors = obj;
+% repeat the clustering 3 times to avoid local minima
+[cluster_idx, cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
+
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+
+segmented_images = cell(1,3);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+
+for k = 1:nColors
+    color = img;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+end
+
+for k = 1:nColors
+    tmp = uint8(segmented_images{k});
+    imwrite(tmp,['segmented_images_0',num2str(k),'.jpg']);
+end
+
+return;
